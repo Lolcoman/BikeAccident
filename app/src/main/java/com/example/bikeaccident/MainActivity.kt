000d92fa3d3f603d682203d758fd46a8f0bdf9fa
Mike <58742308+Lolcoman@users.noreply.github.com>
2022-09-22 22:21:25 +0200

    update graph
    oldest gradle

--- app/src/main/java/com/example/bikeaccident/MainActivity.kt
+++ app/src/main/java/com/example/bikeaccident/MainActivity.kt
@@ -10,12 +10,17 @@ import com.android.volley.toolbox.Volley
 import com.example.bikeaccident.Models.DataResponse
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.components.Legend.LegendForm
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.ViewPortHandler
 import com.google.gson.Gson
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,14 +30,11 @@ class MainActivity : AppCompatActivity() {
             "https://data.brno.cz/datasets/mestobrno::cyklistick%C3%A9-nehody-bike-accidents.geojson?outSR=%7B%22latestWkid%22%3A5514%2C%22wkid%22%3A102067%7D"
 
     lateinit var barList: ArrayList<BarEntry>
-    lateinit var labels: ArrayList<String>
+    //lateinit var year: ArrayList<String>
     lateinit var barChart: BarChart
     lateinit var lineDataSet: BarDataSet
     lateinit var barData: BarData
 
-    fun getFormattedValue(value: Float): String? {
-        return "" + value.toInt()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,31 +47,46 @@ class MainActivity : AppCompatActivity() {
         }
 
         barList = ArrayList()
-        barList.add(BarEntry(2014f,100f))
-        barList.add(BarEntry(2015f,200f))
-        barList.add(BarEntry(2016f,300f))
-        barList.add(BarEntry(2017f,400f))
+        barList.add(BarEntry(8f, 0))
+        barList.add(BarEntry(2f, 1))
+        barList.add(BarEntry(5f, 2))
+        barList.add(BarEntry(20f, 3))
         lineDataSet = BarDataSet(barList,"Počet nehod")
-        lineDataSet.setValueFormatter(getFormattedValue(barList.get(1)))
+        //lineDataSet.setValueFormatter(getFormattedValue(barList.get(1)))
 
+        val year = ArrayList<String>()
+        year.add("2008");
+        year.add("2009");
+        year.add("2010");
+        year.add("2011");
+        val xAxis: XAxis = barChart.xAxis
+        //xAxis.position = XAxisPosition.BOTTOM
+        xAxis.textSize = 15f
+        xAxis.textColor = Color.BLACK
+        xAxis.setDrawAxisLine(true)
+        xAxis.setDrawGridLines(false)
+        barChart.setDescription("")
+        val data = BarData(year,lineDataSet)
+        lineDataSet.setValueFormatter { value, entry, dataSetIndex, viewPortHandler -> value.roundToInt().toString() }
+        //JEN LEGENDA
         val l: Legend = barChart.legend
-        l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-        l.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
-        l.orientation = Legend.LegendOrientation.HORIZONTAL
-        l.setDrawInside(false)
-        l.form = LegendForm.CIRCLE
-        l.formSize = 20f
-        l.textSize = 21f
-        l.xEntrySpace = 10f
-        l.yEntrySpace = 20f
+        //l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        //l.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+        //l.orientation = Legend.LegendOrientation.HORIZONTAL
+        //l.setDrawInside(false)
+        l.form = Legend.LegendForm.SQUARE
+        l.formSize = 15f
+        l.textSize = 20f
+        //l.xEntrySpace = 10f
+        //l.yEntrySpace = 20f
 
-        barData = BarData(lineDataSet)
-        barChart.data = barData
+        //barData = BarData(lineDataSet)
+        barChart.data = data
+        barChart.animateY(3000)
         lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS, 250)
         lineDataSet.valueTextColor = Color.BLACK
         lineDataSet.valueTextSize = 20f
-        barChart.setScaleEnabled(false);
-
+        barChart.setScaleEnabled(false)
     }
 
     //Funkce pro vykreslení grafu
@@ -102,3 +119,4 @@ class MainActivity : AppCompatActivity() {
         queue.add(request)
     }
 }
+
