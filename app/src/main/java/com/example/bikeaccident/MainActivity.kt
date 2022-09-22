e7c85d23830a34453ea50938906546aaef2ba671
Mike <58742308+Lolcoman@users.noreply.github.com>
2022-09-22 14:20:21 +0200

    chart update

--- app/src/main/java/com/example/bikeaccident/MainActivity.kt
+++ app/src/main/java/com/example/bikeaccident/MainActivity.kt
@@ -1,32 +1,74 @@
 package com.example.bikeaccident
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.bikeaccident.Models.DataResponse
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.Legend.LegendForm
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.gson.Gson
-import org.json.JSONObject
-import org.json.JSONTokener
+
 
 class MainActivity : AppCompatActivity() {
+
+
     private val url = "\t\n" +
             "https://data.brno.cz/datasets/mestobrno::cyklistick%C3%A9-nehody-bike-accidents.geojson?outSR=%7B%22latestWkid%22%3A5514%2C%22wkid%22%3A102067%7D"
+
+    lateinit var barList: ArrayList<BarEntry>
+    lateinit var labels: ArrayList<String>
+    lateinit var barChart: BarChart
+    lateinit var lineDataSet: BarDataSet
+    lateinit var barData: BarData
+
+    fun getFormattedValue(value: Float): String? {
+        return "" + value.toInt()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btn = findViewById<Button>(R.id.buttonFetch)
+        barChart = findViewById(R.id.idBarChart)
         btn.setOnClickListener {
-            downloadTask()
+            //downloadTask()
+            barChart.invalidate()
         }
+
+        barList = ArrayList()
+        barList.add(BarEntry(2014f,100f))
+        barList.add(BarEntry(2015f,200f))
+        barList.add(BarEntry(2016f,300f))
+        barList.add(BarEntry(2017f,400f))
+        lineDataSet = BarDataSet(barList,"Počet nehod")
+        lineDataSet.setValueFormatter(getFormattedValue(barList.get(1)))
+
+        val l: Legend = barChart.legend
+        l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+        l.orientation = Legend.LegendOrientation.HORIZONTAL
+        l.setDrawInside(false)
+        l.form = LegendForm.CIRCLE
+        l.formSize = 20f
+        l.textSize = 21f
+        l.xEntrySpace = 10f
+        l.yEntrySpace = 20f
+
+        barData = BarData(lineDataSet)
+        barChart.data = barData
+        lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS, 250)
+        lineDataSet.valueTextColor = Color.BLACK
+        lineDataSet.valueTextSize = 20f
+        barChart.setScaleEnabled(false);
 
     }
 
