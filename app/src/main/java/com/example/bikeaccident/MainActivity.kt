df1467d27909f7955e3b7f2e2d9a3a4b2cf00952
Mike <58742308+Lolcoman@users.noreply.github.com>
2022-09-23 15:29:05 +0200

    update chart
    - fix legend barChart!

--- app/src/main/java/com/example/bikeaccident/MainActivity.kt
+++ app/src/main/java/com/example/bikeaccident/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bikeaccident
 
 import android.graphics.Color
+import android.net.wifi.p2p.WifiP2pManager.ServiceResponseListener
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -20,17 +21,15 @@ import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.utils.ViewPortHandler
 import com.google.gson.Gson
 import java.text.DecimalFormat
+import java.time.Year
 import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
-
-
     private val url = "\t\n" +
             "https://data.brno.cz/datasets/mestobrno::cyklistick%C3%A9-nehody-bike-accidents.geojson?outSR=%7B%22latestWkid%22%3A5514%2C%22wkid%22%3A102067%7D"
 
     lateinit var barList: ArrayList<BarEntry>
-    //lateinit var year: ArrayList<String>
     lateinit var barChart: BarChart
     lateinit var lineDataSet: BarDataSet
     lateinit var barData: BarData
@@ -42,25 +41,43 @@ class MainActivity : AppCompatActivity() {
         val btn = findViewById<Button>(R.id.buttonFetch)
         barChart = findViewById(R.id.idBarChart)
         btn.setOnClickListener {
-            //downloadTask()
             barChart.invalidate()
+            downloadYear()
         }
 
+        val queue = Volley.newRequestQueue(this)
+        val request = StringRequest(Request.Method.GET,url,
+            { response ->
+                val apiData = Gson().fromJson(response, DataResponse::class.java)
+                val features = apiData.features
+
+                fun downloadYear(year: Int): Float{
+                    val featureList = features.filter{
+                        it.properties.rok == year
+                    }
+                    return featureList.size.toFloat()
+                }
+                barList = ArrayList()
+                barList.add(BarEntry(downloadYear(2010), 0))
+                barList.add(BarEntry(downloadYear(2011), 1))
+                barList.add(BarEntry(downloadYear(2012), 2))
+                barList.add(BarEntry(downloadYear(2013), 3))
+                lineDataSet = BarDataSet(barList,"Počet nehod za rok")
+        /*
         barList = ArrayList()
-        barList.add(BarEntry(8f, 0))
+        barList.add(BarEntry(downloadTask(2010), 0))
         barList.add(BarEntry(2f, 1))
         barList.add(BarEntry(5f, 2))
         barList.add(BarEntry(20f, 3))
-        lineDataSet = BarDataSet(barList,"Počet nehod")
+        lineDataSet = BarDataSet(barList,"Počet nehod za rok")
         //lineDataSet.setValueFormatter(getFormattedValue(barList.get(1)))
-
+        */
         val year = ArrayList<String>()
-        year.add("2008");
-        year.add("2009");
         year.add("2010");
         year.add("2011");
+        year.add("2012");
+        year.add("2013");
         val xAxis: XAxis = barChart.xAxis
-        //xAxis.position = XAxisPosition.BOTTOM
         xAxis.textSize = 15f
         xAxis.textColor = Color.BLACK
         xAxis.setDrawAxisLine(true)
@@ -70,52 +87,36 @@ class MainActivity : AppCompatActivity() {
         lineDataSet.setValueFormatter { value, entry, dataSetIndex, viewPortHandler -> value.roundToInt().toString() }
         //JEN LEGENDA
         val l: Legend = barChart.legend
-        //l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-        //l.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
-        //l.orientation = Legend.LegendOrientation.HORIZONTAL
-        //l.setDrawInside(false)
         l.form = Legend.LegendForm.SQUARE
         l.formSize = 15f
         l.textSize = 20f
-        //l.xEntrySpace = 10f
-        //l.yEntrySpace = 20f
-
-        //barData = BarData(lineDataSet)
         barChart.data = data
         barChart.animateY(3000)
         lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS, 250)
         lineDataSet.valueTextColor = Color.BLACK
         lineDataSet.valueTextSize = 20f
         barChart.setScaleEnabled(false)
+
+            },
+            { })
+        queue.add(request)
     }
 
-    //Funkce pro vykreslení grafu
-    private fun setBarChartValues(){
-
-    }
-
-    private fun downloadTask(){
+    private fun downloadYear(){
         val queue = Volley.newRequestQueue(this)
         val request = StringRequest(Request.Method.GET,url,
             { response ->
-                //val data = response.toString()
-                //var jArray = JSONArray(data)
                 val apiData = Gson().fromJson(response, DataResponse::class.java)
                 val features = apiData.features
-
                 val featureList = features.filter{
-                    it.properties.rok == 2014
+                    it.properties.rok == 2000
                 }
-
-                println(featureList.size)
-                //featureList.forEach { println(it) }
-                //val jsonObject = JSONTokener(response).nextValue() as JSONObject
-                //val id = jsonObject.getJSONObject("crs")
-                //val ie = jsonObject.getJSONArray("features").getJSONObject(0).getJSONObject("properties").getString("rok")
-                //Log.i("ROK: ", id)
-                //println(ie)
+                for (rok in features){
+                    println(rok)
+                }
+                //println(features.filter { })
             },
-            {  })
+            { })
         queue.add(request)
     }
 }
