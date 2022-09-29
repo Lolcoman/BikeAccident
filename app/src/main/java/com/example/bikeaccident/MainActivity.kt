86a4f01d74ec14dfd102e4239d94867a5ac1fff2
Mike <58742308+Lolcoman@users.noreply.github.com>
2022-09-29 22:49:15 +0200

    update barChart
    - update barChart
    - years and accidents

--- app/src/main/java/com/example/bikeaccident/MainActivity.kt
+++ app/src/main/java/com/example/bikeaccident/MainActivity.kt
@@ -8,18 +8,16 @@ import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.bikeaccident.Models.DataResponse
+import com.example.bikeaccident.Models.Feature
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
-import com.github.mikephil.charting.utils.ViewPortHandler
 import com.google.gson.Gson
-import java.text.DecimalFormat
+import java.util.*
 import kotlin.math.roundToInt
 
 
@@ -30,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             "https://data.brno.cz/datasets/mestobrno::cyklistick%C3%A9-nehody-bike-accidents.geojson?outSR=%7B%22latestWkid%22%3A5514%2C%22wkid%22%3A102067%7D"
 
     lateinit var barList: ArrayList<BarEntry>
+
     //lateinit var year: ArrayList<String>
     lateinit var barChart: BarChart
     lateinit var lineDataSet: BarDataSet
@@ -41,73 +40,79 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btn = findViewById<Button>(R.id.buttonFetch)
         barChart = findViewById(R.id.idBarChart)
+        downloadTask()
         btn.setOnClickListener {
-            //downloadTask()
-            barChart.invalidate()
+            barChart.fitScreen()
         }
 
-        barList = ArrayList()
-        barList.add(BarEntry(8f, 0))
-        barList.add(BarEntry(2f, 1))
-        barList.add(BarEntry(5f, 2))
-        barList.add(BarEntry(20f, 3))
-        lineDataSet = BarDataSet(barList,"Počet nehod")
-        //lineDataSet.setValueFormatter(getFormattedValue(barList.get(1)))
+    }
 
+    //Funkce pro vykreslení grafu podle roků
+    private fun getYearGraph(apiData: List<Feature>) {
+        barList = ArrayList()
         val year = ArrayList<String>()
-        year.add("2008");
-        year.add("2009");
-        year.add("2010");
-        year.add("2011");
+        var averageAccident: MutableList<Int> = mutableListOf<Int>()
+        val yearNow = Calendar.getInstance().get(Calendar.YEAR);
+        for ((counter, i) in (2010 until yearNow).withIndex()) {
+            val featureList = apiData.filter {
+                it.properties.rok == i
+            }
+            println("Rok: " + i + " Počet nehod: " + featureList.size)
+            barList.add(BarEntry(featureList.size.toFloat(), counter))
+            averageAccident.add(featureList.size)
+            year.add(i.toString())
+        }
+        lineDataSet = BarDataSet(barList,"Průměrně nehod: " + averageAccident.average().toInt())
+        //println(averageAccident.average())
         val xAxis: XAxis = barChart.xAxis
-        //xAxis.position = XAxisPosition.BOTTOM
         xAxis.textSize = 15f
         xAxis.textColor = Color.BLACK
         xAxis.setDrawAxisLine(true)
-        xAxis.setDrawGridLines(false)
+        xAxis.setDrawGridLines(true)
+        xAxis.spaceBetweenLabels = 0
+        lineDataSet.barSpacePercent = 30f
         barChart.setDescription("")
-        val data = BarData(year,lineDataSet)
-        lineDataSet.setValueFormatter { value, entry, dataSetIndex, viewPortHandler -> value.roundToInt().toString() }
-        //JEN LEGENDA
+        val data = BarData(year, lineDataSet)
+        lineDataSet.setValueFormatter { value, entry, dataSetIndex, viewPortHandler ->
+            value.roundToInt().toString()
+        }
+        //Nastavení legendy
         val l: Legend = barChart.legend
-        //l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-        //l.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
-        //l.orientation = Legend.LegendOrientation.HORIZONTAL
-        //l.setDrawInside(false)
         l.form = Legend.LegendForm.SQUARE
         l.formSize = 15f
-        l.textSize = 20f
-        //l.xEntrySpace = 10f
-        //l.yEntrySpace = 20f
-
-        //barData = BarData(lineDataSet)
+        l.textSize = 15f
+        //Nastavení sloupců
         barChart.data = data
         barChart.animateY(3000)
         lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS, 250)
         lineDataSet.valueTextColor = Color.BLACK
-        lineDataSet.valueTextSize = 20f
-        barChart.setScaleEnabled(false)
+        lineDataSet.valueTextSize = 15f
+        //barChart.setScaleEnabled(false)
     }
 
-    //Funkce pro vykreslení grafu
-    private fun setBarChartValues(){
-
-    }
-
-    private fun downloadTask(){
+    private fun downloadTask() {
         val queue = Volley.newRequestQueue(this)
-        val request = StringRequest(Request.Method.GET,url,
+        val request = StringRequest(Request.Method.GET, url,
             { response ->
                 //val data = response.toString()
                 //var jArray = JSONArray(data)
+                //val apiData = Gson().fromJson(response, DataResponse::class.java)
                 val apiData = Gson().fromJson(response, DataResponse::class.java)
                 val features = apiData.features
+                getYearGraph(features)
 
-                val featureList = features.filter{
-                    it.properties.rok == 2014
+                /*
+                for(item in features)
+                {
+                    if (item.properties.rok == 2010)
+                    println(item.properties.rok)
                 }
-
-                println(featureList.size)
+                val featureList = features.filter {
+                   it.properties.rok == 2014
+                }
+                 */
+                //features.forEach()
+                //println(features)
                 //featureList.forEach { println(it) }
                 //val jsonObject = JSONTokener(response).nextValue() as JSONObject
                 //val id = jsonObject.getJSONObject("crs")
@@ -115,7 +120,7 @@ class MainActivity : AppCompatActivity() {
                 //Log.i("ROK: ", id)
                 //println(ie)
             },
-            {  })
+            { })
         queue.add(request)
     }
 }
